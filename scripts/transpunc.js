b187d7dd7791263bfabe93ae0f434da653edfb99
bill-xia <2833145060@qq.com>
2021-10-13 22:35:42 +0800

    transpunc

--- scripts/transpunc.js
+++ scripts/transpunc.js
@@ -11,13 +11,12 @@ function trans_punc(content) {
         replace(/”/g, "” ").
         replace(/（/g, ' (').
         replace(/）/g, ') ').
-        replace(/。/g, '.&nbsp;&nbsp;');
+        replace(/。/g, '. ').
+        replace(/？/g, '? ');
     return content;
 }
 
-if (false) {
-    hexo.extend.filter.register('before_post_render', function(data){
-        data.title = trans_punc(data.title);
-        data.content = trans_punc(data.content);
-    });
-}
+hexo.extend.filter.register('before_post_render', function(data){
+    data.title = trans_punc(data.title);
+    data.content = trans_punc(data.content);
+});
